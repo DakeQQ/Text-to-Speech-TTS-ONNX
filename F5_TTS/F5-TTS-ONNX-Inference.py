900530dec16155f3fb95f7d3c15b6c806b560bb6
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2026-01-07 09:27:40 +0800

    Update F5-TTS-ONNX-Inference.py

--- F5_TTS/F5-TTS-ONNX-Inference.py
+++ F5_TTS/F5-TTS-ONNX-Inference.py
@@ -148,12 +148,6 @@ def list_str_to_idx(
     return text
 
 
-def normalize_to_int16(audio):
-    max_val = np.max(np.abs(audio))
-    scaling_factor = 32767.0 / max_val if max_val > 0 else 1.0
-    return (audio * float(scaling_factor)).astype(np.int16)
-
-
 # ONNX Runtime settings
 onnxruntime.set_seed(RANDOM_SEED)
 session_opts = onnxruntime.SessionOptions()
@@ -226,8 +220,7 @@ out_name_C0 = out_name_C[0].name
 
 # Load the input audio
 print(f"\nReference Audio: {reference_audio}")
-audio = np.array(AudioSegment.from_file(reference_audio).set_channels(1).set_frame_rate(MODEL_SAMPLE_RATE).get_array_of_samples(), dtype=np.float32)
-audio = normalize_to_int16(audio)
+audio = np.array(AudioSegment.from_file(reference_audio).set_channels(1).set_frame_rate(MODEL_SAMPLE_RATE).get_array_of_samples(), dtype=np.int16)
 audio_len = len(audio)
 audio = audio.reshape(1, 1, -1)
 
